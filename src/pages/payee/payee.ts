7bbb08b88b876ec6f1180713942dcd081384faf2
Chakra Bhandari <chakrabhandari.pp@gmail.com>
2017-05-08 21:15:06 +1000

    commit changes

--- src/pages/payee/payee.ts
+++ src/pages/payee/payee.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import {  NavController, NavParams } from 'ionic-angular';
-import {PayeeService} from "../services/payee.service";
+import {  NavController, NavParams, LoadingController } from 'ionic-angular';
+import {Payeeservice} from "../../providers/payeeservice";
+//import {HomePage} from "../home/home";
+//import {Payees} from "../payees/payees";
 
 @Component({
   selector: 'page-payee',
@@ -8,12 +10,61 @@ import {PayeeService} from "../services/payee.service";
 })
 export class Payee {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public payeeservice: PayeeService) {
+  results: Array<any>;
+  loader: any;
+
+  firstName: string = "";
+  middleName: string = "";
+  lastName: string = "";
+  bankDetails: string = "";
+  accountType: string="";
+
+  Bank = {
+    Name: 'key2'
+
+  };
+  Names = [
+    {'Key': "-- select --"},
+    {'Key1': "Kathmandu"},
+    {'Key2': "Kathmandu"},
+    {'Key3': "Kathmandu"},
+    {'Key4': "Kathmandu"},
+    {'Key5': "Kathmandu"}
+  ];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public payeeservice: Payeeservice, public loadingCtrl: LoadingController) {
+
   }
 
   onAddPayee(value: {title: string}) {
-    this.payeeservice.addPayee(value);
     this.navCtrl.pop();
+  }
+
+  public Insert()
+  {
+    this.presentLoading(); // pick customerId from localstorage
+    this.payeeservice.InsertPayee(this.firstName, this.lastName, this.middleName, this.accountType,
+      this.bankDetails,"1").subscribe(
+      data => {
+        this.results = data;
+      },
+      err => {
+        console.log(err);
+        this.loader.dismiss();
+      },
+      () => {
+            console.log('add payee completed');
+            this.loader.dismiss();
+            this.navCtrl.pop();
+         }
+    );
+  }
+
+  presentLoading() {
+    this.loader = this.loadingCtrl.create({
+      content: "Loading..."
+    });
+    this.loader.present();
   }
 
 }
